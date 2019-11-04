c076467eb7ba9684d68fe55329256af3784db7e9
soomin <soomtopia@gmail.com>
2019-11-04 18:44:14 +0900

    week3

--- src/soomin/leet_code/347.Top K Frequent Elements.py
+++ src/soomin/leet_code/347.Top K Frequent Elements.py
@@ -1,5 +1,16 @@
 # heap 
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        print(nums)
+        result = []
+        counter = {}
+        for i in nums:
+            try:
+                counter[i] += 1
+            except:
+                counter[i] = 1
 
+        return heapq.nlargest(k, counter.keys(), key=counter.get) 
 
 # trash
 # class Solution:
